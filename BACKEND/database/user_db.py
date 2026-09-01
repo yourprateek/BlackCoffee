adf19b6edf860ca03b248455b8d4ad16dde12bb1
Saksham Manchanda <sakshu1807@gmail.com>
2026-09-01 12:37:24 +0300

    Update user_db.py to enhance user data retrieval

--- BACKEND/database/user_db.py
+++ BACKEND/database/user_db.py
@@ -1,8 +1,19 @@
 from configurations import profiles_collections
 from pymongo.cursor import Cursor
+from schema.user_schema import User
+from pydantic import EmailStr
 from typing import List
 
-def get_all_data() -> List:
+def get_all_data() -> List[User]:
 
     cursor: Cursor = profiles_collections.find({})
     users = list(cursor)
+
+    return users
+
+def get user_data(email: EmailStr) -> User:
+
+    all_users = get_all_data()
+    final_user = user for user in users if user['email'] == email
+
+    return final_user
