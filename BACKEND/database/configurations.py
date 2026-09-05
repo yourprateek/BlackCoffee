c73ab6fada32dbb15f44b326a8dcbe925924386f
Saksham Manchanda <sakshu1807@gmail.com>
2026-09-05 08:45:00 +0530

    Add certifi import for MongoDB TLS configuration

--- BACKEND/database/configurations.py
+++ BACKEND/database/configurations.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
 from motor.motor_asyncio import AsyncIOMotorClient
+import certifi
 
 uri = "mongodb+srv://sakshu1807_db_user:la7ksVABU1H0cxJz@blackcoffeecluster.7hefs0v.mongodb.net/?appName=BlackCoffeeCluster"
 
